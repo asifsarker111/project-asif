ef5810721521769779a9b8472da0b3c954fc0583
Asif Sarker <gbasifsarker078@gmail.com>
2024-10-23 04:26:14 +0600

    Initial portfolio version completed: Ready for future updates and improvements.

--- js/script.js
+++ js/script.js
@@ -10,3 +10,47 @@ var typed = new Typed(".typing-2",{
     typeSpeed: 100 ,
     backSpeed: 60,
 });
+
+// Showing/Hide FAQs answer
+const faqs = document.querySelectorAll('.faq')
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === 'fa-solid fa-plus'){
+           icon.className = 'fa-solid fa-minus'
+        }
+        else{
+            icon.className = 'fa-solid fa-plus'
+        }
+    })
+})
+
+// Show/Hide nav menu
+const menu = document.querySelector('.nav_menu');
+
+const menuBtn = document.querySelector('#open-menu-btn');
+
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+   menu.style.display = "flex"; 
+   closeBtn.style.display = "inline-block";
+   menuBtn.style.display = "none"
+})
+
+// close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block"
+}
+
+closeBtn.addEventListener('click', closeNav);
+
+// nav color change on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY100)
+})
