c7d8395a36f2f0e41d92ebbb9664ffba326c6224
Asif Sarker <gbasifsarker078@gmail.com>
2024-10-22 21:04:34 +0600

    Set up basic layout and styling for portfolio site

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Type Script JS
+var typed = new Typed(".typing",{
+    strings: ["Web Developer", "Digital Navigator", "Creative Technologist"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+var typed = new Typed(".typing-2",{
+    strings: ["Web Developer", "Digital Navigator", "Creative Technologist"],
+    typeSpeed: 100 ,
+    backSpeed: 60,
+});
